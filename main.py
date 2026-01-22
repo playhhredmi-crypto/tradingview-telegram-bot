b97434050fbbd52b4a0f82b9254ae869df893a2d
playhhredmi-crypto <playhhredmi@gmail.com>
2026-01-23 02:53:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ def webhook():
 def home():
     return "Bot ishlayapti"
 import os
+import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    ilova.yugirish(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
