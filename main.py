429b0acb2f3806f264f29d27f0d601db7a9392b8
playhhredmi-crypto <playhhredmi@gmail.com>
2026-01-23 05:23:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,7 @@ def webhook():
 def home():
     return "Bot ishlayapti"
 import os
-import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
